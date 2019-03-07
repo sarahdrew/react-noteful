dd62370ee08840e736e976ced275ca5cb4750e0a
Sarah Drew <sarahdrew93@gmail.com>
2019-03-07 13:26:12 -0600

    full functionality. not much styling

---
+++ src/NotePageNav/NotePageNav.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import CircleButton from "../CircleButton/CircleButton";
+import "./NotePageNav.css";
+
+export default function NotePageNav(props) {
+  return (
+    <div className="NotePageNav">
+      <CircleButton
+        tag="button"
+        role="link"
+        onClick={() => props.history.goBack()}
+        className="NotePageNav__back-button"
+      >
+        <br />
+        Back
+      </CircleButton>
+      {props.folder && (
+        <h3 className="NotePageNav__folder-name">{props.folder.name}</h3>
+      )}
+    </div>
+  );
+}
+
+NotePageNav.defaultProps = {
+  history: {
+    goBack: () => {}
+  }
+};
